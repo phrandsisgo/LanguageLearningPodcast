0cd949b5174657988a2229d0c05eb528c838253a
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2024-05-24 17:44:12 +0200

    Add get_ISO function to tryOut.py and worked on prompt

--- podcastGenerater.py
+++ podcastGenerater.py
@@ -29,6 +29,7 @@ if wordlist !="":
 #add the language
 storytext = f" The story that you are about to write is written only in the language of  {target_language}. {storytext} {endtext}"
 baselanguage = input("\033[93m" + 'what is the language that you want to have it teached? \n' + "\033[0m").upper()
+
 def podcastgenerator(api_key, text):
     url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + api_key
     headers = {'Content-Type': 'application/json'}
@@ -71,6 +72,7 @@ def betweenPart(baselanguage):
 
 def explainSentence(fullstory, baseLanguage, targetLanguage, wordlist=None, level=None):
     sentences = re.split('[.!?]', fullstory)
+    #sentences = re.findall(r'[^.!?]+[.!?]', fullstory) ## With this the punctuation is included in the sentence.
     promptForExplanation = ""
     combineedExplanations = ""
     print("\033[91m" + str(sentences) + "\033[0m")
@@ -111,9 +113,135 @@ def explainSentence(fullstory, baseLanguage, targetLanguage, wordlist=None, leve
             print("next phrase")
     
     return combineedExplanations
+
+
+def get_ISO(input):
+    print(api_key)
+    url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + api_key
+    headers = {'Content-Type': 'application/json'}
+    starting_prompt = """ 
+    
+    """
+    data = {
+        "contents": [
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": f"""I want you to just return the code of the language with 2 leters of the ISO-Code of the language that is beeing inputted standard. The first input that you need to return is now "japanisch" """
+                    }
+                ]
+            },
+            {
+                "role": "model",
+                "parts": [
+                    {
+                        "text": "JP"
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": f"the input is {input}"
+                    }
+                ]
+            },
+        ]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        if response.json()["candidates"][0]["content"]["parts"][0]["text"] == "JA":
+            response.json()["candidates"][0]["content"]["parts"][0]["text"] = "JP" #found out during testing that the API returns the wrong code for Japanese
+        return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+    else:
+        return f"Error: {response.status_code} - {response.text}" 
+
+def differentiator(story, baseLanguage, targetLanguage):
+    promptForDifferentiator = f"""
+        You're an excellent teacher who explains sentences in an engaging way.
+        Your explanations should be written in {baseLanguage} only.
+        You will be given a sentence and you will explain it in the {baseLanguage} (baseLanguage).
+        Make sure to give the answer with separators (---) when switching languages to separate the languages one from another.
+        Always when using a Separator, you will start the sentence with either a "bl" for baseLanguage (that is the language you're explaining in) or "tl" for targetLanguage (which is the language the sentence is in).
+        You're always explaining the sentence in {baseLanguage}.
+        As soon as you separated the phrase with the --- separator and have a bl then ONLY write in the {baseLanguage}.
+        This is so that later on the base & target Language can be separated from each other.
+        You will exclusively explain the sentence in {baseLanguage}.
+        You will for every sentence focus on a word that could be special in the sentence and explain that as well in the {baseLanguage}.
+        Make sure to write the sentence multiple times in your answer.
+        When you're writing a bl section, make sure to only use the {baseLanguage} and not use the target language.
+        When you're writing a tl section, make sure to only use the targetLanguage that is given by the text and not use the {baseLanguage}.
+        Do not just repeat the sentence in {baseLanguage}, try to come up with a valuable explanation, maybe with an interesting fun fact or something.
+        Keep in mind that after your explanation, the listener will hear the next sentence and the explanation of that sentence.
+
+        Examples:
+        Sentence: 'Я на улице!' / targetLanguage: Russian
+        Explanation: ---RU Я на улице ---EN the Sentence, ---RU Я на улице ---EN means I'm outside. However, ---RU на улице ---EN can be outside but it also can mean on the street. So the sentence can also mean I'm on the street. So, ---RU на улице ---EN has 2 meanings.
+        
+        Sentence: 'Was läuft bei dir Junge?' / targetLanguage: German
+        Explanation: ---DE Was läuft bei dir Junge? ---EN means 'What's up with you, boy?' in English if we want to be literal. But you can also translate it as: 'what's up dude' it is usually used in informal settings. But literally the word ---DE läuft ---EN means walking and ---DE Junge ---EN means boy. One last time the full sentence: ---DE Was läuft bei dir Junge?
+    """
+
+def multiTurnExplainer(sentence, baseIso, targetIso, wordList, level):
+    url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + api_key
+    headers = {'Content-Type': 'application/json'}
+    startPrompt = f"""
+    Hi, you are an excellent teacher for foreign languages and are an expert to distinguish the languages from each other, for a podcast.
+    Your explanations should be written in the desired explain language only. 
+    You will be given a sentence from a story that is written in the target language that the user is currently learning.
+    Your job is to explain the sentence in the explainin language in a way that it sutes the user quite good and he can learn it.
+    Do it in a way, so that the user has to guess a word that might be challenging for someone at his level, do it in a way to describe the word without instantly translating it, But don't do it if the sentence is super easy in his level.
+    If a word from the wordlist is being used in the sentence, you should explain it extra carefully and put more emphasis on it by using it in other sentences as well.
+            I want you to explain the sentence in a way that a language learner who is learning the target language  at the {level} level would understand and also learn something from it.
+            If a word from the wordlist is being used in the sentence, you should explain it extra carefully and put more emphasis on it by using it in other sentences as well.
+            You will be given the following sentence: "{sentence}".
+            If none of the words from the wordlist are used in the sentence, you should explain another word that might be challenging for someone at the {level} level.
+            Here is the wordlist that the listener is currently learning: "{wordList}".
+            Please try to only write 3-5 sentences as a explanation. for the sentence that you are given.
+            When you have done that I will need you to differenciate the text by language. (so that in the production will know which language there is to use)
+    the first sentence that you will be given is: "Hallo wie geht es dir?"
+    
+    """
+    data = {
+        "contents": [
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": startPrompt
+                    }
+                ]
+            },
+            {
+                "role": "model",
+                "parts": [
+                    {
+                        "text": "In the bustling city of Meadow brook, lived a young girl named Sophie. She was a bright and curious soul with an imaginative mind."
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": "Can you set it in a quiet village in 1600s France?"
+                    }
+                ]
+            },
+        ]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error: {response.status_code} - {response.text}" 
     
 
 fertigerPodcast = podcastgenerator(api_key, storyPrompt)
+isoBase = get_ISO(baselanguage)
+isoTarget = get_ISO(target_language)
 print(fertigerPodcast)
 print("\n \n")
 print(Introwriter(storytext, target_language, baselanguage, level)["candidates"][0]["content"]["parts"][0]["text"])
--- tryOut.py
+++ tryOut.py
@@ -87,7 +87,48 @@ def chat_with_model(api_key, starting_message, system_message=None):
             ]
         })
 
+def get_ISO(input):
+    print(api_key)
+    url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + api_key
+    headers = {'Content-Type': 'application/json'}
+    starting_prompt = """ 
+    
+    """
+    data = {
+        "contents": [
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": f"""I want you to just return the code of the language with 2 leters of the ISO-Code of the language that is beeing inputted standard. The first input that you need to return is now "japanisch" """
+                    }
+                ]
+            },
+            {
+                "role": "model",
+                "parts": [
+                    {
+                        "text": "JP"
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": f"the input is {input}"
+                    }
+                ]
+            },
+        ]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error: {response.status_code} - {response.text}" 
+
 # Example usage
-api_key = "YOUR_API_KEY"  # Replace with your actual API key
-starting_message = "Write a story about a magic backpack."
-chat_with_model(api_key, starting_message)
+#api_key = "YOUR_API_KEY"  # Replace with your actual API ke
+print(multiTurn("japanese"))
+#chat_with_model(api_key, starting_message)
